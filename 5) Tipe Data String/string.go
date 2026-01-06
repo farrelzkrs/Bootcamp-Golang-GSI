951ea1788aa592b3bb631b1b804c600f7887230f
farrelzkrs <farrel.z.suryahadi@gmail.com>
2026-01-06 09:03:17 +0700

    belajar tipe data string

---
+++ 5) Tipe Data String/string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Farrel")
+	fmt.Println("Farrel Zikri")
+	fmt.Println("Farrel Lerri")
+	
+	fmt.Println(len("Farrel"))
+	fmt.Println("Farrel Zikri"[1])
+	fmt.Println("Farrel Lerri"[5])
+}
