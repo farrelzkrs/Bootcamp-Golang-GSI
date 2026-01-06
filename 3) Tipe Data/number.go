171071324ee67b4fbaa9805f10b8a81f70b56d39
farrelzkrs <farrel.z.suryahadi@gmail.com>
2026-01-06 08:49:40 +0700

    belajar tipe data number

---
+++ 3) Tipe Data/number.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Satu = ", int8(1))
+	fmt.Println("Dua Ratus Lima Puluh = ", uint8(250))
+	fmt.Println("Sembilan Koma Lima = ", float32(9.5))
+}
