18727141f2ee82374293374bd46aae225b447841
farrelzkrs <farrel.z.suryahadi@gmail.com>
2026-01-07 11:01:42 +0700

    komentar

---
+++ 32) Komentar/com.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Ini adalah komentar
+	fmt.Println("// Ini adalah komentar")
+
+	/**
+	Ini adalah multi-line komentar
+	Tidak terbatas
+	Hai
+	*/
+	fmt.Println("/** Ini adalah multi-line komentar */")
+}
