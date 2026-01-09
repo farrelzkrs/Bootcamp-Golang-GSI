7fe0816cd66d979972d7beae7c93dcea7599d4af
farrelzkrs <farrel.z.suryahadi@gmail.com>
2026-01-09 16:54:19 +0700

    package container ring

--- 52) Package Container/ring/conring.go
+++ 52) Package Container/ring/conring.go
@@ -1,1 +1,20 @@
 package ring
+
+import (
+	"container/ring"
+	"fmt"
+	"strconv"
+)
+
+func Cincin() {
+	var data *ring.Ring = ring.New(5)
+	fmt.Print("-> ")
+	for i := 0; i < data.Len(); i++ {
+		data.Value = "Value " + strconv.FormatInt(int64(i), 10)
+		data = data.Next()
+	}
+
+	data.Do(func(val interface{}) {
+		fmt.Print(val, " -> ")
+	})
+}
--- 52) Package Container/testing.go
+++ 52) Package Container/testing.go
@@ -1,7 +1,11 @@
 package main
 
-import "container_pack/list"
+import (
+	// "container_pack/list"
+	"container_pack/ring"
+)
 
 func main() {
-	list.Data()
+	// list.Data()
+	ring.Cincin()
 }
